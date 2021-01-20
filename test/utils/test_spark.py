887a1790d4885ad4693e8e984f075e38ff64ad3c
alessandro.negrini@axa.it <alessandro2.negrini@gmail.com>
2021-01-20 18:01:40 +0100

    spark class unit test

---
+++ test/utils/test_spark.py
@@ -0,0 +1,32 @@
+import unittest
+from pyspark.sql import SparkSession
+from src.truefilm.utils.spark import Spark
+import os
+
+class TestSpark(unittest.TestCase):
+
+    def test_spark_configurations(self):
+        sparkSession1 = Spark(master="local",
+                              app_name="TrueFilmTest1",
+                              log_level="WARN",
+                              xml_jar_path=os.getcwd()+('/../../resources/jar/spark-xml_2.11-0.5.0.jar'))
+
+        configurations1 = sparkSession1.spark.sparkContext.getConf().getAll()
+
+        for conf in configurations1:
+            if(conf[0] == 'spark.master'):
+                self.assertEqual(conf[1],'local')
+            if (conf[0] == 'spark.app.name'):
+                self.assertEqual(conf[1], 'TrueFilmTest1')
+
+        sparkSession1.stop_spark()
+
+    def test_spark_session_instance_type(self):
+        sparkSession = Spark(master="local",
+                              app_name="TrueFilmTest",
+                              log_level="WARN",
+                              xml_jar_path=os.getcwd()+('/../../resources/jar/spark-xml_2.11-0.5.0.jar'))
+        self.assertIsInstance(sparkSession.spark, SparkSession)
+        sparkSession.stop_spark()
+
+
