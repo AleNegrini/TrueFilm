db31efc09f6b5b45ed70333e3f3ee199490c99b5
alessandro.negrini@axa.it <alessandro2.negrini@gmail.com>
2021-01-21 16:25:17 +0100

    scripts for running tests automatically

---
+++ run-tests-it.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+if [ "$VIRTUAL_ENV" = "" ]; then
+    echo "Virtual environment not set"
+    exit 1
+fi
+
+export ENABLE_LOG=true
+
+TEST_PARAM=$1
+TEST_COMMAND="discover -s test_it -t ."
+if [ "$TEST_PARAM" != "" ]; then
+  TEST_COMMAND="test.$TEST_PARAM"
+fi
+
+eval "python -m unittest $TEST_COMMAND -v"
